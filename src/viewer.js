bc8db9d358b67206393433d724018ba3973befa0
Nicholas Frechette <zeno490@gmail.com>
2020-04-23 13:03:25 -0400

    feat: switch to ACL playback by default

--- src/viewer.js
+++ src/viewer.js
@@ -218,7 +218,7 @@ export class Viewer {
       skeleton: false,
       grid: false,
 
-      animSource: 'Source File', // nfrechette
+      animSource: 'ACL Compressed', // nfrechette
 
       // Lights
       addLights: true,
